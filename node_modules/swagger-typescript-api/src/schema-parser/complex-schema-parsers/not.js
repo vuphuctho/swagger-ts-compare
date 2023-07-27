23a31727ce08a7ddf13bdf9c1ce225a6ca7e7740
Tho Vu <robert_vu947@protonmail.com>
2023-07-27 17:21:44 +0800

    ignore node_modules

--- node_modules/swagger-typescript-api/src/schema-parser/complex-schema-parsers/not.js
+++
@@ -1,9 +0,0 @@
-const { MonoSchemaParser } = require('../mono-schema-parser');
-
-class NotSchemaParser extends MonoSchemaParser {
-  parse() {
-    return this.config.Ts.Keyword.Any;
-  }
-}
-
-module.exports = { NotSchemaParser };
